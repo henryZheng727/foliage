009b84fba10c5762247a5bc347bafeadf1905de8
Henry Zheng <henry.zheng727@gmail.com>
2026-04-01 22:41:06 -0600

    add js-of-ocaml as proof of concept

--- foliage-model/bin/main.ml
+++ foliage-model/bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+open Js_of_ocaml
+open Foliage_model
+
+let () =
+  Js.export "FoliageModel"
+    (object%js
+       method exampleProp =
+         let open Syntax in
+         let prop = Implies (Atom "P", Or (Atom "Q", Not (Atom "R"))) in
+         Js.string (prop_to_string prop)
+    end)
---
+++ foliage-model/lib/syntax.ml
@@ -0,0 +1,16 @@
+(* Propositions *)
+type prop =
+        | Atom of string
+        | And of prop * prop
+        | Or of prop * prop
+        | Implies of prop * prop
+        | Not of prop
+        | False
+
+let rec prop_to_string = function
+  | Atom s -> s
+  | And (a, b) -> "(" ^ prop_to_string a ^ " ∧ " ^ prop_to_string b ^ ")"
+  | Or (a, b) -> "(" ^ prop_to_string a ^ " ∨ " ^ prop_to_string b ^ ")"
+  | Implies (a, b) -> "(" ^ prop_to_string a ^ " → " ^ prop_to_string b ^ ")"
+  | Not a -> "¬" ^ prop_to_string a
+  | False -> "⊥"
